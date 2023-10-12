19a7c5332717cea1bdb5a1aadbafa0df1c93ff49
xlstc110 <xlstc110@gmail.com>
2023-10-12 00:39:41 -0700

    added names to tech balls and centerized the name

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,6 +9,7 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
+          <h3 className="flex justify-center">{technology.name}</h3>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
